77c81520b63f4fb1e80869a8884b07c62008d387
NikitinSemen <pr.pypblrka@gmail.com>
2024-03-21 14:20:34 +0400

    дополнил класс для получения вакансий создание словаря с выбранными значениями

--- source/api_class.py
+++ source/api_class.py
@@ -20,18 +20,11 @@ class HeadHunterApi(ApiClass, ABC):
         self.vacancies = []
 
     def get_vacancies(self):
-        # self.params['text'] = keyword
-
         response = requests.get(self.url, params=self.params)
         vacancies = response.json()['items']
-        self.vacancies.extend(vacancies)
-
+        for i in vacancies:
+            lulu = {'name': i.get('name'), 'salary': i.get('salary'), 'experience': i.get('experience'),
+                    'roles': i.get('roles')}
+            self.vacancies.append(lulu)
         return self.vacancies
 
-
-user_input = input('введите запрос')
-num_input = input('количество желаемых вакансий')
-hh = HeadHunterApi(user_input, num_input)
-lala = hh.get_vacancies()
-for i in lala:
-    print(i)
