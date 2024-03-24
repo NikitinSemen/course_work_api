a87c7973f91e36a45b06b11536de890fe34017ee
NikitinSemen <pr.pypblrka@gmail.com>
2024-03-24 16:31:19 +0400

    добавлена сортировка по зарплате

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ def main():
     num_input = input('количество желаемых вакансий\n')
     hh = HeadHunterApi(user_input, num_input)
     data = hh.get_response()
+    sort_obj_list = sorted(data, key=lambda salar: salar.salary, reverse=True)
 
-    for i in data:
+    for i in sort_obj_list:
         print(
             f'{i.get_name()}\n{i.get_salary()} {i.get_currency()}\n{i.get_experience()}'
             f'\n{i.get_roles()}\n{i.get_requirement()}\n'
