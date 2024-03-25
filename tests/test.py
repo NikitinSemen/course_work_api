c283e68b65e7b9d4a39bc444b9350afa3eb04611
NikitinSemen <pr.pypblrka@gmail.com>
2024-03-25 19:51:47 +0400

    рефакторинг

--- tests/test.py
+++
@@ -1,8 +0,0 @@
-user_input = input('введите запрос\n')
-num_input = input('количество желаемых вакансий\n')
-hh = HeadHunterApi(user_input, num_input)
-lala = hh.get_vacancies()
-
-for i in lala:
-    print(f'{i.get_name()}\n{i.get_salary()}\n{i.get_experience()}\n{i.get_role()}\n{i.get_requirement()}\n'
-          f'{i.get_url()}\n\n\n\n')
